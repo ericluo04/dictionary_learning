fix normalization

dictionary_learning/trainers/matryoshka_batch_top_k.py
@@ -239,7 +239,7 @@ class MatryoshkaBatchTopKTrainer(SAETrainer):
             loss_denom = (residual_BD.float() - residual_mu.float()).pow(2).sum(dim=-1).mean()
             normalized_auxk_loss = l2_loss_aux / loss_denom
 
-            return normalized_auxk_loss.nan_to_num(0.0)
+            return l2_loss_aux.nan_to_num(0.0)
         else:
             self.pre_norm_auxk_loss = -1
             return t.tensor(0, dtype=residual_BD.dtype, device=residual_BD.device)